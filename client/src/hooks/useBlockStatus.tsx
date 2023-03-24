df1a9bef7073de3b90325e572564a9bf1f32b086
Ahmed Semih Erkan <ahmedsemiherkan@gmail.com>
2023-03-24 09:40:56 +0300

    feat: create block and friend status hooks

---
+++ client/src/hooks/useBlockStatus.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import { RootState } from '../redux/store';
+import { checkBlock, setBlocked } from '../services/userService';
+
+const useBlockStatus = ({ id }: any) => {
+    const user = useSelector((state: RootState) => state.auth.user);
+    const [isPending, setIsPending] = useState<boolean>(true);
+    const [isBlocked, setIsBlocked] = useState<boolean>(false);
+
+    useEffect(() => {
+        setIsPending(true);
+        const fetchBlocked = async () => {
+            const result = await checkBlock(user?.id!, id);
+            setIsBlocked(result);
+        }
+
+        fetchBlocked();
+        setIsPending(false);
+    }, [user?.id]);
+
+    const addBlock = () => {
+        setBlocked(user?.id!, id, true);
+        setIsBlocked(true);
+    };
+
+    const removeBlock = () => {
+        setBlocked(user?.id!, id, false);
+        setIsBlocked(false);
+    }
+
+    return { isPending, isBlocked, addBlock, removeBlock };
+}
+
+export default useBlockStatus;
---
+++ client/src/hooks/useFriendStatus.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import { RootState } from '../redux/store';
+import { checkFriend, setFriend } from '../services/userService';
+
+const useFriendStatus = ({ id }: any) => {
+    const user = useSelector((state: RootState) => state.auth.user);
+    const [isFriend, setIsFriend] = useState<boolean>(true);
+    const [isPending, setIsPending] = useState<boolean>(true);
+    
+    useEffect(() => {
+        setIsPending(true);
+        const fetchIsFriend = async () => {
+            const result = await checkFriend(user?.id!, id);
+            setIsFriend(result);
+        }
+        
+        fetchIsFriend();
+        setIsPending(false);
+    }, [user?.id]);
+    
+    const addFriend = () => {
+        setFriend(user?.id!, id, true);
+        setIsFriend(true);
+    }
+
+    const removeFriend = () => {
+        setFriend(user?.id!, id, false);
+        setIsFriend(false);
+    }
+
+    return { isPending, isFriend, addFriend, removeFriend };
+}
+
+export default useFriendStatus;
